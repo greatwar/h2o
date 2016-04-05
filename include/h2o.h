0097cc612e8450680aad0f504477e4001b57ff78
wx <agreatwar@gmail.com>
2016-04-05 10:42:26 +0800

    add data ptr in handler

--- include/h2o.h
+++ include/h2o.h
@@ -120,6 +120,7 @@ typedef struct st_h2o_handler_t {
     void (*on_context_dispose)(struct st_h2o_handler_t *self, h2o_context_t *ctx);
     void (*dispose)(struct st_h2o_handler_t *self);
     int (*on_req)(struct st_h2o_handler_t *self, h2o_req_t *req);
+    void *data;
 } h2o_handler_t;
 
 /**
